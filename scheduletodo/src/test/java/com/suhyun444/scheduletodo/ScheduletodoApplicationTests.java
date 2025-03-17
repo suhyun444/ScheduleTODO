81aea162a6b9057d8076485b04dab1e88ee58fba
Suhyun <67677286+suhyun444@users.noreply.github.com>
2025-03-17 06:25:15 +0000

    Initialize Project

---
+++ scheduletodo/src/test/java/com/suhyun444/scheduletodo/ScheduletodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhyun444.scheduletodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduletodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
