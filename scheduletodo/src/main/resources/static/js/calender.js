215fc215810f0b29f09e42032155e47bb25a6c17
Suhyun <67677286+suhyun444@users.noreply.github.com>
2025-03-20 03:29:28 +0000

    날짜 이동 버튼 추가

--- scheduletodo/src/main/resources/static/js/calender.js
+++ scheduletodo/src/main/resources/static/js/calender.js
@@ -1,10 +1,39 @@
-let monthText = ['JANUARY','FEBRUARY','MARCH','APRIL','MAY','JUNE','JULY','AGUST','SEPTEMBER','OCTOBER','NOVEMBER','DECEMBER'];
+let monthText = ['JAN.','FEB.','MAR.','APR.','MAY.','JUN.','JUL.','AGU.','SEP.','OCT.','NOV.','DEC.'];
 let today = new Date();   
 
-let currentYear = today.getFullYear();
-let currentMonth = today.getMonth();
+monthCurrent();
 
-let monthElement = document.getElementById('calender-info-month-text');
-monthElement.innerText = monthText[currentMonth];
-let yearElement = document.getElementById('calender-info-year-text');
-yearElement.innerText = currentYear;
+function monthDecrease()
+{
+    if(currentMonth == 0 && currentYear == 1) return;
+    currentMonth--;
+    if(currentMonth < 0)
+    {
+        currentMonth = 11;
+        currentYear--;
+    }
+    updateText();
+}
+function monthIncrease()
+{
+    currentMonth++;
+    if(currentMonth > 11)
+    {
+        currentMonth = 0;
+        currentYear++;
+    }
+    updateText();
+}
+function monthCurrent()
+{
+    currentYear = today.getFullYear();
+    currentMonth = today.getMonth(); 
+    updateText();  
+}
+function updateText()
+{
+    let monthElement = document.getElementById('calender-info-month-text');
+    monthElement.innerText = monthText[currentMonth];
+    let yearElement = document.getElementById('calender-info-year-text');
+    yearElement.innerText = currentYear;
+}
