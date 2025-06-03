0078e9c52144cd5fe702c6a4865701382faca091
Suhyun <67677286+suhyun444@users.noreply.github.com>
2025-06-03 05:39:24 +0000

    Github Action Test

--- scheduletodo/src/main/java/com/suhyun444/scheduletodo/ScheduletodoApplication.java
+++ scheduletodo/src/main/java/com/suhyun444/scheduletodo/ScheduletodoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScheduletodoApplication {
-
 	public static void main(String[] args) {	
 		SpringApplication.run(ScheduletodoApplication.class, args);
 	}
